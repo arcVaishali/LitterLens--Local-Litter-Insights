07eb1f7919197ddcf35bac95656b97223530e7f0
vaishali <vaishalirdj14@gmail.com>
2023-08-12 14:28:57 +0530

    modified

--- Home.py
+++ Home.py
@@ -5,8 +5,19 @@ st.set_page_config(
     page_icon="🏠",
 )
 
-st.title("A step towards Litter free World")
-st.markdown("# What we aim for?")
+st.title("Litter Insight and Segregation ")
+st.divider()
+st.markdown("#### About us")
+st.markdown("#### Inspiration")
+st.markdown("##### Case Study - 1 ")
+st.markdown("We saw the success story of [KabadiWallah Connect](), a Chennai based startup, it provided an IoT based point of pickup i.e. an IoT based dustbin which monitored waste collection and notified nearby Kabadiwallahs when it required disposal. Enabling such a system organised the Recyclable Waste Collection in India which is an informal sector which lags structure and also reduced risks that Kabadiwallahs (level-0 waste picker are called so in India) used to take in their daily life to find and segregate waste, thereby improving their livelihoods.") 
+st.divider()
+st.markdown(">What if we could reduce the risks even more that the Kabadiwallahs take to segregating waste by diving knee deep into the waste?")
+st.divider()
+
+st.markdown("##### Case Study - 2 ")
+st.markdown("[Literati]() is a Litter data collection and analysis app based in US. This app has been used by various lawmakers and school administrations in US to get insights on what type of waste are being mostly generated in their city and school campus respectively and how they can reduce it.")
+
 
 
 
