f9809c78e15a4785e7347b0ffd0c989b05fa7f3e
vaishali <vaishalirdj14@gmail.com>
2023-08-12 12:54:49 +0530

    modified

--- MainApp.py
+++ MainApp.py
@@ -5,7 +5,7 @@ from streamlit_option_menu import option_menu
 with st.sidebar:
     selected_sidebar = option_menu(
         menu_title = None,
-        options=["Home page", "Upload Litter", "Retain Litter Info","Contact"],
+        options=["Home page", "Upload Litter", "Waste Segregation Tool","Contact"],
         icons=["house","upload","info","envelope"],
         default_index=0,
 
@@ -13,10 +13,12 @@ with st.sidebar:
 
 if selected_sidebar == "Home page":
     st.title("Litter free World")
-elif selected_sidebar == "Upload Litter":
-    st.title("Upload litter data and Read Litter Data")
-elif selected_sidebar == "Waste Segregation Model":
-    st.title("Upload image of Litter our model will predict its ->>")
+elif selected_sidebar == "Upload Litter and Get Insights ":
+    st.title("Upload Litter Data and Read Litter Data")
+elif selected_sidebar == "Waste Segregation Tool":
+    st.title("Upload image of Litter our model will predict its ")
     st.markdown("- Name ")
     st.markdown("- Type ")
     st.markdown("- other data ") 
+   
+   
