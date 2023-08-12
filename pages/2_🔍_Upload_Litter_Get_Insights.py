c112341c2f5ba3c78ee1ae03e4fee57d7eca325c
Dileep2608 <kadileepjee@gmail.com>
2023-08-12 16:00:43 +0530

    algined buttons

--- pages/2_🔍_Upload_Litter_Get_Insights.py
+++ pages/2_🔍_Upload_Litter_Get_Insights.py
@@ -1,5 +1,8 @@
 import streamlit as st 
 
+def submit():
+    pass
+
 st.set_page_config(
     page_title="Upload and Read",
     page_icon="🔍",
@@ -7,12 +10,35 @@ st.set_page_config(
 
 st.title(":blue[Upload] Litter Data and Read Litter Data") 
 
-col1, col2 = st.columns([0.5,1])
+col1, col2 = st.columns([1,1])
 with col1:
-    upload = st.button("Upload")
+    upload = st.button("Upload", use_container_width=True)
 with col2:
-    camera = st.button("Camera")
-if camera == True:
-    photo = st.camera_input("Take picture")
-else:
-    photo = st.file_uploader("Upload Photo")
+    camera = st.button("Camera", use_container_width=True)
+
+
+def photo():
+    key = True
+    if camera == True:
+        photo = st.camera_input("Take picture")
+        if photo is not None:
+            key = False
+        st.button("Submit", on_click=submit(), disabled=key, use_container_width=True)
+    else:
+        photo = st.file_uploader("Upload image")
+        if photo:
+            st.image(photo)
+        if photo is not None:
+            key = False
+        st.button("Submit", on_click=submit(), disabled=key, use_container_width=True)
+
+photo()
+
+
+
+
+
+
+
+
+
