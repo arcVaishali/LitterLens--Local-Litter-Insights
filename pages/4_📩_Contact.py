2600d2a6b730b153595ced51ecd43464480dd1d9
Maruthi <singhkarthik261@gmail.com>
2023-08-13 18:46:00 +0530

    Changes

--- pages/4_📩_Contact.py
+++ pages/4_📩_Contact.py
@@ -1,13 +1,11 @@
 import streamlit as st 
 import streamlit.components.v1 as components
-from streamlit_extras.app_logo import add_logo
 
 st.set_page_config(
     page_title="Contact",
     page_icon=":envelope_with_arrow:",
 )
 
-add_logo("assests\logo.png", height=180)
 
 components.html(
     """
