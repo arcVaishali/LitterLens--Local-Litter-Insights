2e7c27ceafa990954481ec62af2f1870a70511dd
Maruthi <singhkarthik261@gmail.com>
2023-08-13 08:59:40 +0530

    Added script in login page

--- 1_🖐️_Login.py
+++ 1_🖐️_Login.py
@@ -58,18 +58,7 @@ components.html(
       </form>
     </div>
     <script>
-        var form = document.getElementById('sheetdb-form');
-        form.addEventListener("submit", e => {
-            e.preventDefault();
-            fetch(form.action, {
-                method : "POST",
-                body: new FormData(document.getElementById("sheetdb-form")),
-            }).then(
-                response => response.json()
-            ).then((html) => {
-            alert('Successfully submited uploaded')
-            });
-        });
+        
     </script>
     """,
     height=458,
