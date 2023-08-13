93f446cd8e0c721bc43f34e107d77fea78d36d84
Maruthi <singhkarthik261@gmail.com>
2023-08-13 14:15:48 +0530

    Changes in forms

--- 1_🖐️_Login.py
+++ 1_🖐️_Login.py
@@ -11,45 +11,21 @@ components.html(
     <script src="https://cdn.tailwindcss.com"></script>
 
     <div class="flex flex-col w-full">
-      <h2 class="text-4xl dark:text-white text-black font-medium">Sign up for info</h2>
+      <h2 class="text-6xl dark:text-white text-black font-medium mb-5">Sign up for info</h2>
       <form method="post" id="sheetdb-form" action="https://sheetdb.io/api/v1/9j2teqwz3t3d9?sheet=login">
         <div class="my-4">
-          <label class="block text-xl dark:text-white text-black font-bold mb-2" for="name">
-            Name
-          </label>
-          <input
-            class="h-14 w-full text-white bg-black dark:border-white border-slate-500 border-4 focus:outline-none p-4 rounded-lg"
-            id="name"
-            name="data[name]"
-            type="text"
-            required
-          />
+            <h2 class="text-xl font-bold mb-2 dark:text-white text-black">Name</h2>
+            <input required type="text" class="h-14 w-full text-white bg-black dark:border-white border-slate-500 border-4 focus:outline-none p-4 rounded-lg" name="data[name]">
         </div>
         <div class="my-4">
-          <label class="block text-xl dark:text-white text-black font-bold mb-2" for="location">
-            Loacation
-          </label>
-          <input
-            class="h-14 w-full text-white bg-black dark:border-white border-slate-500 border-4 focus:outline-none p-4 rounded-lg"
-            id="location"
-            name="data[loacation]"
-            type="text"
-            required
-          />
+            <h2 class="text-xl font-bold mb-2 dark:text-white text-black">Location</h2>
+            <input required type="text" class="h-14 w-full text-white bg-black dark:border-white border-slate-500 border-4 focus:outline-none p-4 rounded-lg" name="data[location]">
         </div>
         <div class="my-4">
-          <label class="block text-xl dark:text-white text-black font-bold mb-2" for="locality">
-            Locality
-          </label>
-          <input
-            class="h-14 w-full text-white bg-black dark:border-white border-slate-500 border-4 focus:outline-none p-4 rounded-lg"
-            id="locality"
-            name="data[locality]"
-            type="text"
-            required
-          />
+            <h2 class="text-xl font-bold mb-2 dark:text-white text-black">Locality</h2>
+            <input required type="text" class="h-14 w-full text-white bg-black dark:border-white border-slate-500 border-4 focus:outline-none p-4 rounded-lg" name="data[locality]">
         </div>
-        <button class="text-xl font-bold text-black p-4 bg-lime-400 rounded-full w-fit" type="submit">
+        <button class="text-xl mb-4 font-bold text-black p-4 bg-lime-400 rounded-full w-fit" type="submit">
           Submit
         </button>
       </form>
@@ -69,5 +45,5 @@ components.html(
         });
     </script>
     """,
-    height=442,
+    height=480,
 )
